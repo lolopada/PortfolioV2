be671042911c90094d145ea713d9405a5c4dca14
sainton <sainton@localhost.local>
2025-02-02 00:06:48 +0100

    supp cv, fix et ajout  poisson

--- assets/JavaScript/fish-animation.js
+++ assets/JavaScript/fish-animation.js
@@ -1,27 +1,58 @@
 const fish1Path = {
-    curviness: 1.25,
+    curviness: 1.7,
     autoRotate: true,
     values: [
-        {x:"15vw", y:0},
-        {x:"17vw", y:"7vh"},
-        {x:"19vw", y:"14vh"}, 
-        {x:"19vw", y:"21vh"}, 
-        {x:"21vw", y:"30vh"},
-        {x:"23vw", y:"35vh"},
-        {x:"25vw", y:"40vh"},
-        {x:"28vw", y:"45vh"}
+        {x:"0vw", y:"10.5vh"},
+        {x:"15vw", y:"20.5vh"},
+        {x:"30vw", y:"34.5vh"},
+        {x:"15vw", y:"45vh"},
+        {x:"30vw", y:"60vh"},
+        {x:"40vw", y:"57vh"},
+        {x:"55vw", y:"40vh"},
+        {x:"80vw", y:"58vh"},
+        {x:"70vw", y:"67vh"},
+        {x:"75vw", y:"43vh"},
+        {x:"90vw", y:"40vh"},
+        {x:"120vw", y:"55vh"}
     ]
 };
 
-
-
+const fish2Path = {
+    curviness: 1.7,
+    autoRotate: true,
+    values: [
+        {x:"10vw", y:"0vh"},
+        {x:"15vw", y:"10vh"},
+        {x:"25vw", y:"-23vh"},
+        {x:"38vw", y:"-30vh"},
+        {x:"44vw", y:"-48vh"},
+        {x:"37vw", y:"-57vh"},
+        {x:"26vw", y:"-46vh"},
+        {x:"21vw", y:"-36vh"},
+        {x:"23vw", y:"-30vh"},
+        {x:"36vw", y:"-30vh"},
+        {x:"45vw", y:"-25vh"},
+        {x:"50vw", y:"-18vh"},
+        {x:"58vw", y:"-7vh"},
+        {x:"78vw", y:"-10vh"},
+        {x:"87vw", y:"-23vh"},
+        {x:"84vw", y:"-40vh"},
+        {x:"74vw", y:"-34vh"},
+        {x:"79vw", y:"-26vh"},
+        {x:"100vw", y:"-31vh"},
+        {x:"120vw", y:"-55vh"}
+    ]
+};
 
 const tweenFish = new TimelineLite();
 
-tweenFish.add(
-    TweenLite.to(".fish-1", 4, {
-        delay:0,
+tweenFish.add([
+    TweenLite.to(".fish-1", 8, {
         bezier: fish1Path,
         ease: Power1.easeInOut
+    }),
+    TweenLite.to(".fish-2", 11, {
+        bezier: fish2Path,
+        ease: Power1.easeInOut
     })
-);
+]);
